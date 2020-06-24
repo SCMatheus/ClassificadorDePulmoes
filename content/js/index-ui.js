48410ea75ec076f5c2d5ba2f601217dcb552dbbf
carlos eduardo <carloseduardo057@gmail.com>
2020-06-24 09:58:29 -0300

    saving changes

--- content/js/index-ui.js
+++ content/js/index-ui.js
@@ -21,7 +21,6 @@
                 $('#myform').on('submit',(function(e) {
                     e.preventDefault();
                     var formData = new FormData(this);
-            
                     $.ajax({
                         type:'POST',
                         url: $(this).attr('action'),
